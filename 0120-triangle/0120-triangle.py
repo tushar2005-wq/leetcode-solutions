e6f688a2358ff4ad0b6cea863597ac494a58a115
Tushar Srivastava <srivastavatushar548@gmail.com>
2026-06-02 09:03:56 +0530

    Time: 6 ms (38.7%), Space: 12.9 MB (84.05%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,17 +1,11 @@
 class Solution(object):
     def minimumTotal(self, triangle):
         ht=len(triangle)
-        dp = [[None]*ht for _ in range(ht)]
-        def f(i,j):
-            if i==(ht-1):
-                return triangle[i][j]
-            if dp[i][j] is not None:
-                return dp[i][j]
-            down=triangle[i][j]+ f(i+1,j)
-            diag=triangle[i][j] + f(i+1,j+1)
-            dp[i][j]=min(down,diag)
-            return min(down,diag)
-        return f(0,0)
+        dp=triangle[-1]
+        for i in range(ht-2,-1,-1):
+            for j in range(len(triangle[i])):
+                dp[j]=triangle[i][j] + min(dp[j],dp[j+1])
+        return dp[0]
         """
         :type triangle: List[List[int]]
         :rtype: int
