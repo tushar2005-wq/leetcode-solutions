0a87a003d529c5053569adc29fd03644e3adf055
Tushar Srivastava <srivastavatushar548@gmail.com>
2026-06-13 22:08:18 +0530

    Time: 4 ms (88.59%), Space: 12.3 MB (57.63%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def romanToInt(self, s):
+        roman={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        total=0
+        prev=0
+        for ch in reversed(s):
+            value=roman[ch]
+            if value<prev:
+                total-=value
+            else:
+                total+=value
+            prev=value
+        return total
+        """
+        :type s: str
+        :rtype: int
+        """
+        
