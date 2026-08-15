768ac0735c19cd306a6db3d45611dd1470df7b79
Tushar Srivastava <srivastavatushar548@gmail.com>
2026-08-15 11:18:42 +0530

    Time: 8 ms (11.52%), Space: 19.8 MB (76.69%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,6 +1,11 @@
 class Solution(object):
     def reverseString(self, s):
-        s.reverse()
+        l=0
+        r=len(s)-1
+        while l<=r:
+            s[l],s[r]=s[r],s[l]
+            l+=1
+            r-=1
         return s
         """
         :type s: List[str]
