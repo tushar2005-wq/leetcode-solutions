fef43974c575199445c9dc2fc5394db57ce5c4a0
Tushar Srivastava <srivastavatushar548@gmail.com>
2026-09-10 15:20:58 +0530

    Time: 7 ms (53.41%), Space: 12.7 MB (12.72%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -4,13 +4,13 @@ class Solution(object):
         nums=list(map(str,nums)) #because we are sorting for strings
         def compare(a,b):
             if a+b>b+a:
-                return -1
+                return -1    #it means 1st will come first
             elif a+b<b+a:
-                return 1
+                return 1    #it means 1st will come later
             else:
                 return 0
         nums.sort(key=cmp_to_key(compare))
-        if nums[0]=="0":
+        if nums[0]=="0":  #if largest element is 0 then whole must be 0
             return "0"
         return "".join(nums)
         
