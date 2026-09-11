1204c9b2204dc5c28e62962c0788afe1d86c2f3a
Tushar Srivastava <srivastavatushar548@gmail.com>
2026-09-11 22:52:34 +0530

    Time: 416 ms (32.57%), Space: 12.6 MB (6.27%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,8 +1,8 @@
 class Solution(object):
-    def longestPalindrome(self, s):
+    def longestPalindrome(self, s): #expand from the centre
         n=len(s)
         max_ans=""
-        for i in range(n):
+        for i in range(n): # case for the odd length
             low=i
             high=i
             while low>=0 and high<n and s[low]==s[high]:
@@ -12,7 +12,7 @@ class Solution(object):
                 high+=1
             low=i
             high=i+1
-            while low>=0 and high<n and s[low]==s[high]:
+            while low>=0 and high<n and s[low]==s[high]: #case for the even length
                 if high-low+1>len(max_ans):
                     max_ans=s[low:high+1]
                 low-=1
