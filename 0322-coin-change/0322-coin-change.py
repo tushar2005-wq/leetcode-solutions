e932f0641dc7285a3f7b6a82e7ecc146b0746c6d
Tushar Srivastava <srivastavatushar548@gmail.com>
2026-09-11 11:06:30 +0530

    Time: 2516 ms (5%), Space: 85.3 MB (5%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,17 +3,23 @@ class Solution(object):
         n=len(coins)
         dp={}
         def solve(idx,amount):
+            #base case
             if idx==0:
                 if amount%coins[0]==0:
-                    return amount // coins[0]
+                    return amount//coins[0]
                 return float('inf')
+            #check for dp value
             if (idx,amount) in dp:
                 return dp[(idx,amount)]
+            #not taking condition
             not_take=solve(idx-1,amount)
+            #taking condition
             take=float('inf')
-            if coins[idx]<=amount:
+            if amount>=coins[idx]:
                 take=1+solve(idx,amount-coins[idx])
+            #putting the value in dp
             dp[(idx,amount)]=min(take,not_take)
+            #returning the final value
             return dp[(idx,amount)]
         ans=solve(n-1,amount)
         return -1 if ans==float('inf') else ans
